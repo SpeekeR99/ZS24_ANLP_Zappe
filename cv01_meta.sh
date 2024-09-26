51b14e0d71f4925140397f30839687918654621c
SpeekeR <DominikZappe1999@seznam.cz>
2024-09-26 16:26:09 +0200

    MetaCentrum scripts prepared

---
+++ cv01_meta.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#PBS -q default@pbs-m1.metacentrum.cz
+#PBS -l walltime=2:0:0
+#PBS -l select=1:ncpus=4:mem=20gb:scratch_local=10gb
+#PBS -N my_awesome_job
+
+CONTAINER=/cvmfs/singularity.metacentrum.cz/NGC/PyTorch:21.03-py3.SIF
+PYTHON_SCRIPT=/storage/plzen1/home/zapped99/anlp/cv01/run_cv01.py
+
+singularity run $CONTAINER python $PYTHON_SCRIPT -model $m -optimizer $o -lr $l -dp $d
---
+++ run_cv01_meta.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+models=("dense" "cnn")
+optimizers=("sgd" "adam")
+lrs=(0.1 0.01 0.001 0.0001 0.00001)
+dps=(0 0.1 0.3 0.5)
+
+for m in "${models[@]}"; do
+    for o in "${optimizers[@]}"; do
+        for l in "${lrs[@]}"; do
+            for d in "${dps[@]}"; do
+                echo $m $o $l $d
+                source ./cv01_meta.sh
+            done
+        done
+    done
+done
