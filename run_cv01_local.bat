dd66c90fab658cec77a3dae4493b8a22d4743fd5
SpeekeR <DominikZappe1999@seznam.cz>
2024-09-26 15:55:08 +0200

    Local grid search batch script done

---
+++ run_cv01_local.bat
@@ -0,0 +1,17 @@
+@Echo off
+
+set models=("dense" "cnn")
+set optimizers=("sgd" "adam")
+set lrs=(0.1 0.01 0.001 0.0001 0.00001)
+set dps=(0 0.1 0.3 0.5)
+
+for %%m in %models% do (
+    for %%o in %optimizers% do (
+        for %%l in %lrs% do (
+            for %%d in %dps% do (
+                echo %%m %%o %%l %%d
+                python train.py -model %%m -optimizer %%o -lr %%l -dp %%d
+            )
+        )
+    )
+)
