91f7487d8459c0bcfd6410fd10a10041674c2325
SpeekeR <DominikZappe1999@seznam.cz>
2024-10-18 23:34:33 +0200

    MetaCentrum is pain sometimes

--- cv02_meta.sh
+++ cv02_meta.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q default@pbs-m1.metacentrum.cz
 #PBS -l walltime=2:0:0
-#PBS -l select=1:ncpus=4:mem=32gb:scratch_local=32gb
+#PBS -l select=1:ncpus=4:mem=32gb:scratch_local=32gb:plzen=True
 #PBS -N anlp_cv02
 
 DATADIR=/storage/plzen1/home/zapped99/anlp/anlp-2024_zappe_dominik
