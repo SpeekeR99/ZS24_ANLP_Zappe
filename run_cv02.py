4de253072780c36c58b50ca41908434d711b6f04
SpeekeR <DominikZappe1999@seznam.cz>
2024-10-19 00:37:27 +0200

    True/False with bash and python argparser is something else

--- run_cv02.py
+++ run_cv02.py
@@ -32,9 +32,9 @@ if __name__ == '__main__':
     parser.add_argument('-batch_size', type=int, default=1000)
     parser.add_argument('-lr', type=float, default=0.01)
     parser.add_argument('-optimizer', type=str, default="sgd")
-    parser.add_argument('-random_emb', type=bool, default=False)
-    parser.add_argument('-emb_training', type=bool, default=False)
-    parser.add_argument('-emb_projection', type=bool, default=True)
+    parser.add_argument('-random_emb', type=str, default="false")
+    parser.add_argument('-emb_training', type=str, default="false")
+    parser.add_argument('-emb_projection', type=str, default="true")
     parser.add_argument('-final_metric', type=str, default="neural")
     parser.add_argument('-vocab_size', type=int, default=20_000)
     parser.add_argument('-lr_scheduler', type=str, default="multiStepLR")
@@ -45,9 +45,9 @@ if __name__ == '__main__':
         "batch_size": args.batch_size,
         "lr": args.lr,
         "optimizer": args.optimizer,
-        "random_emb": args.random_emb,
-        "emb_training": args.emb_training,
-        "emb_projection": args.emb_projection,
+        "random_emb": args.random_emb == "true",
+        "emb_training": args.emb_training == "true",
+        "emb_projection": args.emb_projection == "true",
         "final_metric": args.final_metric,
         "vocab_size": args.vocab_size,
         "lr_scheduler": args.lr_scheduler
